bcc5799c67446e866c9fb81c24a814b1bd6b703a
Artem <artemidqwerty69@gmail.com>
2024-08-24 16:24:38 +0300

    настройка sidebar

---
+++ src/components/sideBar/sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './sidebar.module.scss'
+
+type Props = {
+
+}
+
+export const Sidebar: React.FC<Props> = ({}) => {
+    return ( 
+        <aside className={styles.sidebar}>
+
+        </aside>
+    );
+}
